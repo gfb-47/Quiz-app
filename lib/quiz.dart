4adce96f3a3322523be0040e10bed8d1b5a85e8d
gfb-47 <gabrieelfbrito@gmail.com>
2020-04-12 21:07:55 -0300

    first commit

---
+++ lib/quiz.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final Function answerQuestion;
+
+  Quiz({
+    @required this.questions,
+    @required this.answerQuestion,
+    @required this.questionIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //this hold a list,   and a list of widgets, we could do: children: <Widget> but tjere is type inference
+        Question(
+          questions[questionIndex]['questionText'],
+        ),
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(() => answerQuestion(answer['score']), answer['text']);
+        }).toList()
+      ],
+    );
+  }
+}
